5b0766b323d0762f7c67a853ae5e356a29455801
hky1999 <976929993@qq.com>
2024-12-18 06:10:53 +0000

    Deploying to gh-pages from @ arceos-org/riscv_plic@a6e3acdf4fa6ec5249e3adb115515b6903d00382 🚀

---
+++ riscv_plic/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Plic"],"trait":["HartContext","InterruptSource"]};
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["riscv_plic",["",[],["lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[33]}
