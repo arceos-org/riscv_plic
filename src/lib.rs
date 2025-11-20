be7776f3c790c2155718c610b437cce664b440e7
朝倉水希 <asakuramizu111@gmail.com>
2025-11-20 12:24:14 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 //! RISC-V Platform-Level Interrupt Controller
 //! <https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc>
+
 #![no_std]
-#![feature(const_option)]
-#![feature(const_nonnull_new)]
 
 use core::num::NonZeroU32;
 use core::ptr::NonNull;
 
 use tock_registers::{
-    interfaces::{Readable, Writeable},
+    fields::Field,
+    interfaces::{ReadWriteable, Readable, Writeable},
     register_structs,
     registers::{ReadOnly, ReadWrite},
 };
+
+mod hart;
+pub use hart::*;
 
 /// See §1.
 const SOURCE_NUM: usize = 1024;
@@ -21,42 +24,30 @@ const CONTEXT_NUM: usize = 15872;
 const U32_BITS: usize = u32::BITS as usize;
 
 register_structs! {
-  #[allow(non_snake_case)]
   ContextLocal {
     /// Priority Threshold
     /// - The base address of Priority Thresholds register block is located at 4K alignment starts from offset 0x200000.
-    (0x0000 => PriorityThreshold: ReadWrite<u32>),
+    (0x0000 => priority_threshold: ReadWrite<u32>),
     /// Interrupt Claim/complete Process
     /// - The Interrupt Claim Process register is context based and is located at (4K alignment + 4) starts from offset 0x200000.
-    (0x0004 => InterruptClaimComplete: ReadWrite<u32>),
+    (0x0004 => interrupt_claim_complete: ReadWrite<u32>),
     (0x0008 => _reserved_0),
     (0x1000 => @END),
   }
 }
 
 register_structs! {
-  #[allow(non_snake_case)]
-  InterruptEnableCtxX {
-    /// Priority Threshold
-    /// - The base address of Priority Thresholds register block is located at 4K alignment starts from offset 0x200000.
-    (0x00 => InterruptSources: [ReadWrite<u32>; SOURCE_NUM / U32_BITS]),
-    (0x80 => @END),
-  }
-}
-
-register_structs! {
-  #[allow(non_snake_case)]
   PLICRegs {
     /// Interrupt Source Priority #0 to #1023
-    (0x000000 => InterruptPriority: [ReadWrite<u32>; SOURCE_NUM]),
+    (0x000000 => interrupt_priority: [ReadWrite<u32>; SOURCE_NUM]),
     /// Interrupt Pending Bit of Interrupt Source #0 to #N
     /// 0x001000: Interrupt Source #0 to #31 Pending Bits
     /// ...
     /// 0x00107C: Interrupt Source #992 to #1023 Pending Bits
-    (0x001000 => InterruptPending: [ReadOnly<u32>; 0x20]),
+    (0x001000 => interrupt_pending: [ReadOnly<u32>; SOURCE_NUM / U32_BITS]),
     (0x001080 => _reserved_0),
     /// Interrupt Enable Bit of Interrupt Source #0 to #1023 for 15872 contexts
-    (0x002000 => InterruptEnableCtxX: [InterruptEnableCtxX; CONTEXT_NUM]),
+    (0x002000 => interrupt_enable: [[ReadWrite<u32>; SOURCE_NUM / U32_BITS]; CONTEXT_NUM]),
     (0x1F2000 => _reserved_1),
     /// 4096 * 15872 = 65011712(0x3e000 00) bytes
     /// Priority Threshold for 15872 contexts
@@ -64,29 +55,9 @@ register_structs! {
     /// Interrupt Claim Process for 15872 contexts
     /// - The Interrupt Claim Process register is context based and is located at (4K alignment + 4) starts from offset 0x200000.
     /// - The Interrupt Completion registers are context based and located at the same address with Interrupt Claim Process register, which is at (4K alignment + 4) starts from offset 0x200000.
-    (0x200000 => Contexts: [ContextLocal; CONTEXT_NUM]),
+    (0x200000 => contexts: [ContextLocal; CONTEXT_NUM]),
     (0x4000000 => @END),
   }
-}
-
-/// Trait for enums of external interrupt source.
-///
-/// See §1.4.
-pub trait InterruptSource {
-    /// The identifier number of the interrupt source.
-    fn id(self) -> NonZeroU32;
-}
-
-/// A hart context is a given privilege mode on a given hart.
-///
-/// See §1.1.
-pub trait HartContext {
-    /// See §6.
-    ///
-    /// > How PLIC organizes interrupts for the contexts (Hart and privilege mode)
-    /// > is out of RISC-V PLIC specification scope, however it must be spec-out
-    /// > in vendor’s PLIC specification.
-    fn index(self) -> usize;
 }
 
 /// Platform-Level Interrupt Controller.
@@ -99,19 +70,23 @@ unsafe impl Sync for Plic {}
 
 impl Plic {
     /// Create a new instance of the PLIC from the base address.
-    pub const fn new(base: *mut u8) -> Self {
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `base` is a valid base address of PLIC.
+    pub const unsafe fn new(base: usize) -> Self {
         Self {
-            base: NonNull::new(base).unwrap().cast(),
+            base: unsafe { NonNull::new_unchecked(base as *mut _) },
         }
     }
 
     /// Initialize the PLIC by context, setting the priority threshold to 0.
-    pub fn init_by_context<C>(&mut self, context: C)
+    pub fn init_by_context<C>(&self, context: C)
     where
         C: HartContext,
     {
-        self.regs().Contexts[context.index()]
-            .PriorityThreshold
+        self.regs().contexts[context.index()]
+            .priority_threshold
             .set(0);
     }
 
@@ -129,94 +104,76 @@ impl Plic {
     ///
     /// See §4.
     #[inline]
-    pub fn set_priority<S>(&self, source: S, value: u32)
-    where
-        S: InterruptSource,
-    {
-        self.regs().InterruptPriority[source.id().get() as usize].set(value);
+    pub fn set_priority(&self, source: u32, value: u32) {
+        self.regs().interrupt_priority[source as usize].set(value);
     }
 
     /// Gets priority for interrupt `source`.
     ///
     /// See §4.
     #[inline]
-    pub fn get_priority<S>(&self, source: S) -> u32
-    where
-        S: InterruptSource,
-    {
-        self.regs().InterruptPriority[source.id().get() as usize].get()
+    pub fn get_priority(&self, source: u32) -> u32 {
+        self.regs().interrupt_priority[source as usize].get()
     }
 
     /// Probe maximum level of priority for interrupt `source`.
     ///
     /// See §4.
     #[inline]
-    pub fn probe_priority_bits<S>(&self, source: S) -> u32
-    where
-        S: InterruptSource,
-    {
-        let source = source.id().get() as usize;
-        self.regs().InterruptPriority[source].set(!0);
-        self.regs().InterruptPriority[source].get()
+    pub fn probe_priority_bits(&self, source: u32) -> u32 {
+        self.regs().interrupt_priority[source as usize].set(!0);
+        self.regs().interrupt_priority[source as usize].get()
     }
 
     /// Check if interrupt `source` is pending.
     ///
     /// See §5.
     #[inline]
-    pub fn is_pending<S>(&self, source: S) -> bool
-    where
-        S: InterruptSource,
-    {
-        let (group, index) = parse_group_and_index(source.id().get() as usize);
-        self.regs().InterruptPending[group].get() & (1 << index) != 0
+    pub fn is_pending(&self, source: u32) -> bool {
+        let (group, field) = parse_group_and_field(source as usize);
+        self.regs().interrupt_pending[group].read(field) != 0
     }
 
     /// Enable interrupt `source` in `context`.
     ///
     /// See §6.
     #[inline]
-    pub fn enable<S, C>(&self, source: S, context: C)
+    pub fn enable<C>(&self, source: u32, context: C)
     where
-        S: InterruptSource,
         C: HartContext,
     {
         let context = context.index();
-        let (group, index) = parse_group_and_index(source.id().get() as usize);
+        let (group, field) = parse_group_and_field(source as usize);
 
-        let value = self.regs().InterruptEnableCtxX[context].InterruptSources[group].get();
-        self.regs().InterruptEnableCtxX[context].InterruptSources[group].set(value | 1 << index);
+        self.regs().interrupt_enable[context][group].modify(field.val(1));
     }
 
     /// Disable interrupt `source` in `context`.
     ///
     /// See §6.
     #[inline]
-    pub fn disable<S, C>(&self, source: S, context: C)
+    pub fn disable<C>(&self, source: u32, context: C)
     where
-        S: InterruptSource,
         C: HartContext,
     {
         let context = context.index();
-        let (group, index) = parse_group_and_index(source.id().get() as usize);
+        let (group, field) = parse_group_and_field(source as usize);
 
-        let value = self.regs().InterruptEnableCtxX[context].InterruptSources[group].get();
-        self.regs().InterruptEnableCtxX[context].InterruptSources[group].set(value & !(1 << index));
+        self.regs().interrupt_enable[context][group].modify(field.val(0));
     }
 
     /// Check if interrupt `source` is enabled in `context`.
     ///
     /// See §6.
     #[inline]
-    pub fn is_enabled<S, C>(&self, source: S, context: C) -> bool
+    pub fn is_enabled<C>(&self, source: u32, context: C) -> bool
     where
-        S: InterruptSource,
         C: HartContext,
     {
         let context = context.index();
-        let (group, index) = parse_group_and_index(source.id().get() as usize);
+        let (group, field) = parse_group_and_field(source as usize);
 
-        self.regs().InterruptEnableCtxX[context].InterruptSources[group].get() & (1 << index) != 0
+        self.regs().interrupt_enable[context][group].read(field) != 0
     }
 
     /// Get interrupt threshold in `context`.
@@ -227,8 +184,8 @@ impl Plic {
     where
         C: HartContext,
     {
-        self.regs().Contexts[context.index()]
-            .PriorityThreshold
+        self.regs().contexts[context.index()]
+            .priority_threshold
             .get()
     }
 
@@ -240,8 +197,8 @@ impl Plic {
     where
         C: HartContext,
     {
-        self.regs().Contexts[context.index()]
-            .PriorityThreshold
+        self.regs().contexts[context.index()]
+            .priority_threshold
             .set(value);
     }
 
@@ -254,8 +211,8 @@ impl Plic {
         C: HartContext,
     {
         let context = context.index();
-        self.regs().Contexts[context].PriorityThreshold.set(!0);
-        self.regs().Contexts[context].PriorityThreshold.get()
+        self.regs().contexts[context].priority_threshold.set(!0);
+        self.regs().contexts[context].priority_threshold.get()
     }
 
     /// Claim an interrupt in `context`, returning its source.
@@ -272,8 +229,8 @@ impl Plic {
         C: HartContext,
     {
         NonZeroU32::new(
-            self.regs().Contexts[context.index()]
-                .InterruptClaimComplete
+            self.regs().contexts[context.index()]
+                .interrupt_claim_complete
                 .get(),
         )
     }
@@ -282,19 +239,19 @@ impl Plic {
     ///
     /// See §9.
     #[inline]
-    pub fn complete<C, S>(&self, context: C, source: S)
+    pub fn complete<C>(&self, context: C, source: NonZeroU32)
     where
         C: HartContext,
-        S: InterruptSource,
     {
-        self.regs().Contexts[context.index()]
-            .InterruptClaimComplete
-            .set(source.id().get());
+        self.regs().contexts[context.index()]
+            .interrupt_claim_complete
+            .set(source.get());
     }
 }
 
-fn parse_group_and_index(source: usize) -> (usize, usize) {
+fn parse_group_and_field(source: usize) -> (usize, Field<u32, ()>) {
     let group = source / U32_BITS;
     let index = source % U32_BITS;
-    (group, index)
+    let field = Field::<u32, ()>::new(0b1, index);
+    (group, field)
 }
